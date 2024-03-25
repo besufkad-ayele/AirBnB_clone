2297b3d58041e4ef4d0552c1a91638dd06a6783d
besufkad-ayele <ayebesufkad@gmail.com>
2024-03-25 06:28:16 +0300

    3323

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
 import models
 from uuid import uuid4
 from datetime import datetime
@@ -7,12 +6,6 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
@@ -27,16 +20,11 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
+       
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
@@ -44,6 +32,5 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
